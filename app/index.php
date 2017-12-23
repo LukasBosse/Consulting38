9e66e656217fd6ded507b272111944148c9f3913
Lukas Bosse <LukasBosse95@gmail.com>
2017-12-23 16:10:48 +0100

    Inital upload

---
+++ app/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: L15905
+ * Date: 23.12.2017
+ * Time: 15:57
+ */
+
+?>
+
+<html>
+<head>
+<title>ConsultingSite38</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: L15905
+ * Date: 23.12.2017
+ * Time: 15:57
+ */
+
+header("Location: app/");
