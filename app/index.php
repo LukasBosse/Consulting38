2923ef1008ce885e1777f7d2299c3aa391cde264
Lukas Bosse <LukasBosse95@gmail.com>
2017-12-23 21:20:02 +0100

    Added basic

--- app/index.php
+++ app/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: L15905
+ * User: Lukas Bosse
  * Date: 23.12.2017
  * Time: 15:57
  */
@@ -10,9 +10,72 @@
 
 <html>
 <head>
-<title>ConsultingSite38</title>
+    <title>FinanzFreunde38</title>
+    <!-- Meta Tags -->
+    <meta name="author" content="Consulting38">
+    <meta name="publisher" content="Lorem ipsum">
+    <meta name="copyright" content="Lorem ipsum">
+    <meta name="description" content="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata">
+    <meta name="keywords" content="Finanzkompass, Versicherung, WWK, Beratung, Consulting, Information, Hilfe, Rente, Investment, Video, ">
+    <meta name="page-topic" content="Branche Produkt">
+    <meta name="page-type" content="Bericht Reportage">
+    <meta name="audience" content="Alle"><meta http-equiv="content-language" content="de">
+    <meta name="robots" content="index, follow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <!-- Local Stylesheets -->
+
+    <!-- External Stylesheets -->
+    <link type="text/css" href="assets/css/Materialize/materialIcons.css" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="assets/css/Materialize/materialize.min.css"  media="screen,projection"/>
+    <link rel="stylesheet" href="assets/css/Bootstrap/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Custom Stylesheets -->
+    <link type="text/css" href="resources/css/main.css" rel="stylesheet">
+
 </head>
 <body>
+
+    <!-- Navbar -->
+    <nav class="custom-navbar">
+        <div class="nav-wrapper">
+            <a href="#!" id="logo" class="brand-logo">FinanzFreunde38</a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="sass.html">Home</a></li>
+                <li><a href="badges.html">Finanzkompass</a></li>
+                <li><a href="collapsible.html">Kontakt</a></li>
+                <li><a href="mobile.html">Impressum</a></li>
+            </ul>
+            <ul class="side-nav" id="mobile-demo">
+                <li><a href="sass.html">Home</a></li>
+                <li><a href="badges.html">Finanzkompass</a></li>
+                <li><a href="collapsible.html">Kontakt</a></li>
+                <li><a href="mobile.html">Impressum</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Video Content -->
+    <div class="videoContainer">
+        <div class="well videoIntro">
+            <video class="videoController" controls>
+                <source src="movie.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+        </div>
+        <div class="well videoChips">
+            <div style="text-align: left;">
+                <b>Author:</b> Henning Vitzke | <b>Gesehen:</b> 12 Mal | <b>Rating:</b> <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+            </div>
+        </div>
+    </div>
+
+    <!-- Local JavaScript -->
+    <script type="text/javascript" src="assets/js/Jquery/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="assets/js/Materialize/materialize.min.js"></script>
+    <script src="assets/js/Bootstrap/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 </body>
 </html>
