80c078a9b5413ef8427f9f2e5753cc0237489ee9
Lukas Bosse <LukasBosse95@gmail.com>
2017-12-26 19:54:17 +0100

    Rebrand

--- app/index.php
+++ app/index.php
@@ -26,80 +26,68 @@
     <!-- Local Stylesheets -->
 
     <!-- External Stylesheets -->
-    <link type="text/css" href="assets/css/Materialize/materialIcons.css" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="assets/css/Materialize/materialIcons.css">
     <link type="text/css" rel="stylesheet" href="assets/css/Materialize/materialize.min.css"  media="screen,projection"/>
-    <link rel="stylesheet" href="assets/css/Bootstrap/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome-4.7.0/css/font-awesome.min.css">
     <!-- Custom Stylesheets -->
     <link type="text/css" href="resources/css/main.css" rel="stylesheet">
 
 </head>
 <body>
 
-    <!-- Navbar -->
-    <nav class="custom-navbar">
-        <div class="nav-wrapper">
-            <a href="#!" id="logo" class="brand-logo">FinanzFreunde38</a>
-            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-            <ul class="right hide-on-med-and-down">
-                <li><a href="sass.html">Home</a></li>
-                <li><a href="badges.html">Finanzkompass</a></li>
-                <li><a href="collapsible.html">Kontakt</a></li>
-                <li><a href="mobile.html">Impressum</a></li>
-            </ul>
-            <ul class="side-nav" id="mobile-demo">
-                <li><a href="sass.html">Home</a></li>
-                <li><a href="badges.html">Finanzkompass</a></li>
-                <li><a href="collapsible.html">Kontakt</a></li>
-                <li><a href="mobile.html">Impressum</a></li>
-            </ul>
-        </div>
-    </nav>
+    <div class="pageContent">
 
-    <!-- Video Content -->
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-3">
-                <!-- Customer Content -->
-                <div class="panel panel-primary customerContentLeft">
-                    <div class="panel-heading">Infos</div>
-                    <div class="panel-body">
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                    </div>
-                </div>
+        <div class=""></div>
+
+    <!-- Navbar -->
+
+    <div class="burger">
+        <i class="fa fa-bars fa-3x" aria-hidden="true"></i>
+    </div>
+
+    <div id="myCarousel" class="carousel slide" data-ride="carousel" style="height: 100vh;">
+
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="http://www.chroface.de/user-content/templates/base/downloads/hintergrundbilder/sonnenuntergang-winter-landschaft-natur-hintergrundbild.jpg">
             </div>
-            <div class="col-sm-6">
-                <div class="videoContainer">
-                    <div class="well videoIntro">
-                        <video class="videoController" controls>
-                            <source src="movie.mp4" type="video/mp4">
-                            <source src="movie.ogg" type="video/ogg">
-                            Your browser does not support the video tag.
-                        </video>
-                    </div>
-                    <div class="well videoChips">
-                        <div style="text-align: left;">
-                            <b>Author:</b> Henning Vitzke | <b>Gesehen:</b> 12 Mal | <b>Rating:</b> <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                        </div>
-                    </div>
-                </div>
+
+            <div class="item">
+                <img src="http://hd-hintergrundbilder.com/natur/de-zon-komt-op.jpg">
             </div>
-            <div class="col-sm-3">
-                <!-- Customer Content -->
-                <div class="panel panel-primary customerContentRight">
-                    <div class="panel-heading">Kontakt</div>
-                    <div class="panel-body">
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                    </div>
-                </div>
+
+            <div class="item">
+                <img src="http://www.chroface.de/user-content/templates/base/downloads/hintergrundbilder/sonnenuntergang-winter-landschaft-natur-hintergrundbild.jpg">
             </div>
         </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
+
+    </div>
+
 
     <!-- Local JavaScript -->
     <script type="text/javascript" src="assets/js/Jquery/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="assets/js/Materialize/materialize.min.js"></script>
-    <script src="assets/js/Bootstrap/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!--<script type="text/javascript" src="assets/js/Materialize/materialize.min.js"></script>-->
+    <script type="text/javascript" src="assets/js/Bootstrap/bootstrap.min.js"></script>
 
 </body>
 </html>
